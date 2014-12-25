96e46b1745a8ef0bc026bb96353f7ee335a830f5
Pride Leong <lykling.lyk@gmail.com>
2014-12-25 10:25:36 +0800

    add demo

---
+++ demo/src/main.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2014 All rights reserved.
+ *
+ * @file main.js
+ * @author Pride Leong<lykling.lyk@gmail.com>
+ */
+
+window.performance.mark('performance_static_js_sync_loaded');
+
+define(function (require, exports, module) {
+    var config = require('fc-monitor/config');
+    var logger = require('fc-monitor/logger');
+    var recorder = require('fc-monitor/recorder');
+    var fc = require('fc-core');
+    var context = {
+        userid: 39,
+        optis: 39,
+        ulevelid: 39,
+        token: 'ncl20c2ijk'
+    };
+    var staticMarks = [
+        'performance_static_html_parse',
+        'performance_static_css_loaded',
+        'performance_static_js_sync_loaded',
+        'performance_static_js_async_loaded',
+        'performance_static_basicInfo_start',
+        'performance_static_basicInfo_finish',
+        'performance_static_er_inited'
+    ];
+    var materialListMarks = [
+        'performance_materialList_start_0',
+        'performance_materialList_data_loaded_1',
+        'performance_materialList_view_rendered_2',
+        'performance_materialList_finish_3'
+    ];
+    window.performance.mark('performance_static_js_async_loaded');
+    window.performance.mark('performance_static_basicInfo_start');
+    window.performance.mark('performance_static_basicInfo_finish');
+
+    recorder.init(context);
+
+    window.performance.mark('performance_static_er_inited');
+
+    config.config({
+        resourceWatchingList: [
+            /(src|asset)\/.*(initer|main).*\.js/g
+        ],
+        staticMarks: staticMarks,
+        timelineMarks: [].concat(staticMarks, materialListMarks)
+    });
+
+    window.setTimeout(function () {
+        require('fc-monitor/performance/static').measure();
+        require('fc-monitor/performance/resource').measure();
+        var itemKey = 'materialList';
+        require('fc-monitor/performance').mark(itemKey, 'start');
+        require('fc-monitor/performance').mark(itemKey, 'data_loaded');
+        require('fc-monitor/performance').mark(itemKey, 'view_rendered');
+        require('fc-monitor/performance').mark(itemKey, 'finish').measure(itemKey);
+        fc.setImmediate(function () {
+            logger.dump({method: 'console'});
+        });
+    }, 1000);
+});
+
+require(['main']);
